d55c0b5d3bb8608700acf47c22ec1a406a042b3d
UrsidaeWasTaken <ursidaemail@gmail.com>
2021-08-21 20:17:59 +0100

    Updated About Page • Added content about project

--- grow-react/src/pages/about.js
+++ grow-react/src/pages/about.js
@@ -2,13 +2,16 @@ function About() {
     return (
         <div
         style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '90vh'
+            margin:'0 30em',
+            textAlign: 'center',
         }}
       >
-        <h1>About</h1>
+        <h1 style={{margin: '1em'}}>
+            About
+        </h1>
+        <p>
+            GROW is a fun little project to get a better understanding of various frameworks and languages by building a full-stack framework from scratch. It is made with Javascript/React for the frontend, C#/ASP.NET API for the backend, and Microsoft Server as a database. I've used some other applications like Postman for API testing and Unsplash for images.
+        </p>
       </div>
     )
 }
