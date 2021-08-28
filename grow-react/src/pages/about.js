9680274cd4e0aa4d8de78db40ca19db9cec28134
UrsidaeWasTaken <ursidaemail@gmail.com>
2021-08-28 18:50:41 +0100

    Fixed About Page • CSS margin & description fixed

--- grow-react/src/pages/about.js
+++ grow-react/src/pages/about.js
@@ -1,16 +1,11 @@
 function About() {
     return (
-        <div
-        style={{
-            margin:'0 30em',
-            textAlign: 'center',
-        }}
-      >
-        <h1 style={{margin: '1em'}}>
+        <div>
+        <h1 style={{margin: '1em', textAlign: 'center'}}>
             About
         </h1>
-        <p>
-            GROW is a fun little project to get a better understanding of various frameworks and languages by building a full-stack framework from scratch. It is made with Javascript/React for the frontend, C#/ASP.NET API for the backend, and Microsoft Server as a database. I've used some other applications like Postman for API testing and Unsplash for images.
+        <p style={{margin:'0 12vw', textAlign: 'center'}}>
+            GROW is a fun little project to get a better understanding of various frameworks and languages by building a full-stack website from scratch. It is made with Javascript (React) for the frontend, C# (ASP.NET API) for the backend, and Microsoft Server as the database. I've used some other applications like Postman for API testing and Unsplash for images.
         </p>
       </div>
     )
